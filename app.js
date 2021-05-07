14514bc87603093f365e8d95ba656ae9fe16eb72
NrujalSharma <snrujal@gmail.com>
2021-05-08 01:03:27 +0530

    Added null check for allowedOrigins env variable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins && allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
